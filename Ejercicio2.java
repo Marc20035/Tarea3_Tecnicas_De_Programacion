d5e659c83daef3a46f0282f7c5c76b6c594c0547
tHeCreator <marcsabalete@gmail.com>
2023-02-14 17:54:53 +0000

    Ejer1_2

---
+++ Ejercicio2.java
@@ -0,0 +1,17 @@
+/*Modificar el algoritmo anterior para mostrar un mensaje según la temperatura:Temperatura Mensaje t < 2 Riesgo de hielo 2 ≤ t < 15 Hace frío 15 ≤ t < 30 Buena temperatura t ≥ 30 Demasiado calor*/
+class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce la temperatura: ");
+        int temp = sc.nextInt();
+        if (temp < 2) {
+            System.out.println("Riesgo de hielo");
+        } else if (temp >= 2 && temp < 15) {
+            System.out.println("Hace frío");
+        } else if (temp >= 15 && temp < 30) {
+            System.out.println("Buena temperatura");
+        } else {
+            System.out.println("Demasiado calor");
+        }
+    }
+}
